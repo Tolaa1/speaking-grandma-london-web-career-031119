3170f1770e7e225c462173ba908e5efb882352a2
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-02-15 02:47:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,5 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(phrase)
-  if phrase != phrase.upcase()
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase != phrase.upcase()
- return "I LOVE YOU TOO PUMPKIN!"
-  else phrase != phrase.upcase()
- 
+
 speak_to_grandma 
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
