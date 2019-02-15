7368cd49478c9c99f3141b18206ae1edb160acb7
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-02-15 02:50:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,8 +4,9 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
-    
-  else
+  elsif phrase != phrase.upcase()
+ return "I LOVE YOU TOO PUMPKIN!"
+   else
     return "NO, NOT SINCE 1938!"
   end
 end
